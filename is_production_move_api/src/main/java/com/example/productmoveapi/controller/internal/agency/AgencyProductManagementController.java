4a12ea47516993e2203832d0a6e95a4ea401f13c
Nguyễn Thái Bình <binhtp27112000@gmail.com>
2022-12-26 02:30:16 +0700

    change error product to customer service

--- is_production_move_api/src/main/java/com/example/productmoveapi/controller/internal/agency/AgencyProductManagementController.java
+++ is_production_move_api/src/main/java/com/example/productmoveapi/controller/internal/agency/AgencyProductManagementController.java
@@ -1,6 +1,7 @@
 package com.example.productmoveapi.controller.internal.agency;
 
 import com.example.productmoveapi.dto.request.product_request.AddProductListRequest;
+import com.example.productmoveapi.dto.request.product_request.ChangeProductRequest;
 import com.example.productmoveapi.dto.request.product_request.SaleProductRequest;
 import com.example.productmoveapi.response.GeneralResponse;
 import com.example.productmoveapi.service.agency.AgencyProductManagementService;
@@ -79,5 +80,16 @@ public class AgencyProductManagementController {
   public ResponseEntity<GeneralResponse<Object>> returnProductToCustomer(
       @Valid @RequestBody AddProductListRequest addProductListRequest) {
     return agencyProductManagementService.returnProductToCustomer(addProductListRequest);
+  }
+
+  @GetMapping("/warranty/error")
+  public ResponseEntity<GeneralResponse<Object>> getProductErrorFromWarranty() {
+    return agencyProductManagementService.getProductErrorFromWarranty();
+  }
+
+  @PostMapping("/warranty/error")
+  public ResponseEntity<GeneralResponse<Object>> changeProductErrorToCustomer(
+      @Valid @RequestBody ChangeProductRequest changeProductRequest) {
+    return agencyProductManagementService.changeProductErrorToCustomer(changeProductRequest);
   }
 }
