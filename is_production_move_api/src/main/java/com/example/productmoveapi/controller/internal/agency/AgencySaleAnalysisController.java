9bdf6530f3208d38cf91be1d0392e12a51ed08f6
Nguyễn Thái Bình <binhtp27112000@gmail.com>
2022-12-28 02:27:58 +0700

    sales analysis for agency

---
+++ is_production_move_api/src/main/java/com/example/productmoveapi/controller/internal/agency/AgencySaleAnalysisController.java
@@ -0,0 +1,37 @@
+package com.example.productmoveapi.controller.internal.agency;
+
+import com.example.productmoveapi.dto.request.static_request.AgencySaleAnalysisRequest;
+import com.example.productmoveapi.response.GeneralResponse;
+import com.example.productmoveapi.service.agency.AgencySaleAnalysisService;
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Binh Nguyen Thai at 01:26 on 28/12/2022
+ */
+@CrossOrigin(value = "*")
+@Slf4j
+@RestController
+@RequestMapping("/agency/sale/analysis")
+@PreAuthorize("hasAuthority('agency')")
+public class AgencySaleAnalysisController {
+
+  private final AgencySaleAnalysisService agencySaleAnalysisService;
+
+  public AgencySaleAnalysisController(AgencySaleAnalysisService agencySaleAnalysisService) {
+    this.agencySaleAnalysisService = agencySaleAnalysisService;
+  }
+
+  @GetMapping
+  public ResponseEntity<GeneralResponse<Object>> saleAnalysis(
+      @Valid @RequestBody AgencySaleAnalysisRequest agencySaleAnalysisRequest) {
+    return agencySaleAnalysisService.saleAnalysis(agencySaleAnalysisRequest);
+  }
+}
---
+++ is_production_move_api/src/main/java/com/example/productmoveapi/dto/request/static_request/AgencySaleAnalysisRequest.java
@@ -0,0 +1,16 @@
+package com.example.productmoveapi.dto.request.static_request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import lombok.Data;
+
+/**
+ * @author Binh Nguyen Thai at 01:31 on 28/12/2022
+ */
+@Data
+public class AgencySaleAnalysisRequest {
+
+  @NotBlank
+  @Pattern(message = "Invalid option", regexp = "^[0-2]$")
+  private String option;
+}
