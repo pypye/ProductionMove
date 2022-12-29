40070c27fb2ba3dfa7eed7970e559b5791b9a9e0
Nguyễn Thái Bình <binhtp27112000@gmail.com>
2022-12-29 20:24:27 +0700

    product report for agency service

---
+++ is_production_move_api/src/main/java/com/example/productmoveapi/controller/internal/agency/statistic/AgencyProductReportController.java
@@ -0,0 +1,40 @@
+package com.example.productmoveapi.controller.internal.agency.statistic;
+
+import com.example.productmoveapi.dto.request.statistic_request.StaticByStatusYearQuarterMonthRequest;
+import com.example.productmoveapi.response.GeneralResponse;
+import com.example.productmoveapi.service.agency.statistic.AgencyProductReportService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Binh Nguyen Thai at 19:56 on 29/12/2022
+ */
+@CrossOrigin(value = "*")
+@Slf4j
+@RestController
+@RequestMapping("/agency/product/statistic")
+@PreAuthorize("hasAuthority('agency')")
+public class AgencyProductReportController {
+
+  private final AgencyProductReportService agencyProductReportService;
+
+  @Autowired
+  public AgencyProductReportController(
+      AgencyProductReportService agencyProductReportService) {
+    this.agencyProductReportService = agencyProductReportService;
+  }
+
+  @GetMapping("/{statusId}/{option}")
+  public ResponseEntity<GeneralResponse<Object>> getProductByStatusYearQuarterMonth(
+      @PathVariable(name = "statusId") String statusId, @PathVariable(name = "option") String option) {
+    return agencyProductReportService.getProductByStatusYearQuarterMonth(
+        new StaticByStatusYearQuarterMonthRequest(statusId, option));
+  }
+}
